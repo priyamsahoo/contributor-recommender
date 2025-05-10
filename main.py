b39bd43ce6ad9499da067191a3643d171db4d4ef
Priyam Sahoo <priyamsahoo.26@gmail.com>
2025-05-10 14:40:19 -0500

    bring the main file to the root of the project

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
-from github import list_github_issues
+
+from source.github import list_github_issues
 from source.get_contributors_BM25 import find_contributors
-from utils import parse_github_url, save_issues_to_file
-from keyword_extraction import process_single_issue, load_issues
+from source.utils import parse_github_url, save_issues_to_file
+from source.keyword_extraction import process_single_issue, load_issues
 
 def main():
     load_dotenv()
@@ -13,7 +14,10 @@ def main():
     try:
         owner, repo = parse_github_url(repo_url)
         issues = list_github_issues(owner, repo, token=token)
-        filename = f"../outputs/{repo}_issues.json"
+
+        filename = f"{os.getcwd()}/outputs/{repo}_issues.json"
+
+        
         save_issues_to_file(issues, filename)
         #keywords extraction
         choice = input("Do you want to extract keywords from a specific issue now? (y/n): ").strip().lower()
@@ -21,9 +25,10 @@ def main():
         if choice == "y":
             issues = load_issues(filename)
             issue_number = int(input("Enter the issue number you want to process: "))
-            output_filename = f"../outputs/{repo}_issues_with_keywords.json"
+            output_filename = f"{os.getcwd()}/outputs/{repo}_issues_with_keywords.json"
             process_single_issue(issues, issue_number, output_filename)
 
+            # find contributors based on PR
             print("\nFinding top 5 most relevant contributors…")
             top_users = find_contributors(
                 owner,
@@ -36,6 +41,8 @@ def main():
             print("Top 5 contributors:")
             for i, user in enumerate(top_users, start=1):
                 print(f"{i}. {user}")
+
+            # find 
         else:
             print("Keyword extraction skipped.")
     except Exception as e:
